09b5d934e8d807c2b617e16a37d0e73fe3767fad
Hasan0734 <jahidhasan2856@gmail.com>
2023-10-14 20:28:22 +0600

    added tag button

--- components/Home/HomeBanner.tsx
+++ components/Home/HomeBanner.tsx
@@ -14,6 +14,11 @@ const HomeBanner = () => {
                   after:absolute after:w-full after:bg-gradient-to-b after:from-black/0 after:to-black/25 after:bottom-0 after:left-0 after:right-0 after:h-4/5"
                 style={{ backgroundImage: `url(${blog.image})` }}
               >
+                <div className="px-2 py-2">
+                  <span className="bg-primeblue px-2 py-0.5 rounded text-white text-xs">
+                    Design
+                  </span>
+                </div>
                 <div className="absolute bottom-0 left-0 right-0 px-5 py-3 z-10">
                   <h3 className="text-white text-2xl font-semibold">
                     {blog.title}
