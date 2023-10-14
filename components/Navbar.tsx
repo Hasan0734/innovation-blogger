ee64d4fd01e97f30e6ad065ab1e2945e8d7c2153
Hasan0734 <jahidhasan2856@gmail.com>
2023-10-14 15:53:34 +0600

    added search bar and sheet for mobile device

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,48 +1,96 @@
 "use client";
-import { Menu, Search } from "lucide-react";
+import { Menu, Search, X } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import navItems from "../lib/navbar.json";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Input } from "./ui/input";
 
 const Navbar = () => {
+  const [isSearch, setIsSearch] = useState(false);
+
   return (
-    <nav className="container mx-auto flex items-center justify-between py-3">
-      <div className="flex items-center gap-5">
-        <div className="flex items-center gap-2">
-          <button className="block md:hidden px-1.5 py-1.5 rounded">
-            <Menu width={24} />
-          </button>
-          <div>
-            <Link href={"/"}>
-              <img
-                className="w-28 md:w-40"
-                src="assets/roxifylogo.png"
-                alt="logo"
-              />
-            </Link>
+    <Sheet>
+      <nav className="container mx-auto flex items-center gap-3 justify-between py-3">
+        {isSearch && (
+          <div className="flex-grow">
+            <Input className="w-full  !focus-visible:ring-0" />
           </div>
-        </div>
-        <div className="hidden md:block">
-          <ul className="flex items-center gap-2">
-            {navItems.map((item) => (
-              <li key={item.id}>
-                <Link
-                  href={item.link}
-                  className="hover:text-primeblue font-semibold text-base px-4 py-2"
-                >
-                  {item.name}
+        )}
+        {!isSearch && (
+          <div className="flex items-center gap-5">
+            <div className="flex items-center gap-2">
+              <SheetTrigger asChild>
+                <button className="block md:hidden px-1.5 py-1.5 rounded">
+                  <Menu width={24} />
+                </button>
+              </SheetTrigger>
+
+              <div>
+                <Link href={"/"}>
+                  <img
+                    className="w-28 md:w-40"
+                    src="assets/roxifylogo.png"
+                    alt="logo"
+                  />
                 </Link>
-              </li>
-            ))}
-          </ul>
+              </div>
+            </div>
+            <div className="hidden md:block">
+              <ul className="flex items-center gap-2">
+                {navItems.slice(0, 8).map((item) => (
+                  <li key={item.id}>
+                    <Link
+                      href={item.link}
+                      className="hover:text-primeblue font-semibold text-base px-2 py-2"
+                    >
+                      {item.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        )}
+
+        <div>
+          <button
+            onClick={() => setIsSearch(!isSearch)}
+            className="px-2 py-1.5 sm:py-2 bg-gray-100 rounded"
+          >
+            {!isSearch ? <Search width={20} /> : <X width={20}/>}
+          </button>
         </div>
-      </div>
-      <div>
-        <button className="px-2 py-1.5 sm:py-2 bg-gray-100 rounded">
-          <Search width={20} />
-        </button>
-      </div>
-    </nav>
+      </nav>
+
+      <SheetContent side={"left"}>
+        <SheetHeader>
+          <SheetTitle>
+            <img className="w-2/4" src="assets/roxifylogo.png" alt="logo" />
+          </SheetTitle>
+        </SheetHeader>
+
+        <ul className="mt-4">
+          {navItems.map((item) => (
+            <li key={item.id} className="my-2">
+              <Link
+                href={item.link}
+                className="hover:text-primeblue font-semibold text-base py-2"
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </SheetContent>
+    </Sheet>
   );
 };
 
